952709ffcae63a24796ace3dbcacefc8e8a3cca9
doug-luce <douglasluce@Douglass-MacBook-Pro.local>
2018-07-20 21:42:26 -0700

    Changed index to use HashRouter to fix navigation error.

--- src/index.js
+++ src/index.js
@@ -2,12 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 // import registerServiceWorker from './registerServiceWorker';
-import { BrowserRouter as Router } from "react-router-dom";
+// import { BrowserRouter as Router } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 ReactDOM.render(
-    <Router>
+    <HashRouter>
         <App />
-    </Router>,
+    </HashRouter>,
 
 document.getElementById('root'));
 // registerServiceWorker();
