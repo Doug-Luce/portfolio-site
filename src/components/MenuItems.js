1935c19ea1582f7b6c5284ace34095cd0cb3bb16
doug-luce <douglasluce@Douglass-MacBook-Pro.local>
2018-07-20 22:12:46 -0700

    Changed links in menu.

--- src/components/MenuItems.js
+++ src/components/MenuItems.js
@@ -1,28 +1,51 @@
+// import React from 'react';
+// import MenuItemsStyle from './styles/MenuItemsStyles';
+// import { withRouter } from 'react-router-dom';
+
+// class MenuItems extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.clickHandler = this.clickHandler.bind(this);
+//   }
+//   clickHandler(path) {
+//     this.props.history.push(path);
+//     this.props.onClick();
+//   }
+//   render() {
+//     return(
+//       <MenuItemsStyle>
+//         <ul>
+//           <li onClick={() => this.clickHandler('/')} className="home"><span id="home">HOME</span></li>
+//           <li onClick={() => this.clickHandler('/about')} className="about"><a href="/about"><span id="about">ABOUT</span></a></li>
+//           <li onClick={() => this.clickHandler('/projects')} className="projects"><a href="/projects"><span id="projects">PROJECTS</span></a></li>
+//           <li onClick={() => this.clickHandler('/contact')} className="contact"><a href="/contact"><span id="contact">CONTACT</span></a></li>
+//         </ul>
+//       </MenuItemsStyle>
+//     );
+//   }
+// }
+
+// export default withRouter(MenuItems);
+
 import React from 'react';
 import MenuItemsStyle from './styles/MenuItemsStyles';
 import { withRouter } from 'react-router-dom';
-import createHistory from 'history/createBrowserHistory'
-
-
+import { Link } from 'react-router-dom';
 
 class MenuItems extends React.Component {
   constructor(props) {
     super(props);
     this.clickHandler = this.clickHandler.bind(this);
   }
-  clickHandler(path) {
-    this.props.history = createHistory();
-    this.props.history.push(path);
-    this.props.onClick();
-  }
+
   render() {
     return(
       <MenuItemsStyle>
         <ul>
-          <li onClick={() => this.clickHandler('/')} className="home"><span id="home">HOME</span></li>
-          <li onClick={() => this.clickHandler('/about')} className="about"><a href="/about"><span id="about">ABOUT</span></a></li>
-          <li onClick={() => this.clickHandler('/projects')} className="projects"><a href="/projects"><span id="projects">PROJECTS</span></a></li>
-          <li onClick={() => this.clickHandler('/contact')} className="contact"><a href="/contact"><span id="contact">CONTACT</span></a></li>
+          <li className="home"><Link to="/"><span id="home">HOME</span></Link></li>
+          <li className="about"><Link to ="/about"><span id="about">ABOUT</span></Link></li>
+          <li className="projects"><Link to ="/projects"><span id="projects">PROJECTS</span></Link></li>
+          <li className="contact"><Link to ="/contact"><span id="contact">CONTACT</span></Link></li>
         </ul>
       </MenuItemsStyle>
     );
