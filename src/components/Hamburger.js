6df50115972af012066267ac22fb848fea617b24
doug-luce <douglasluce@Douglass-MacBook-Pro.local>
2018-07-20 21:22:46 -0700

    Fixed link to be https for SSL.

--- src/components/Hamburger.js
+++ src/components/Hamburger.js
@@ -4,7 +4,7 @@ class Hamburger extends React.Component {
   render() {
     return (
       // conditionally render the hamburger icon or the close icon depending on the state passed in as props.
-      this.props.toggle ? <img onClick={this.props.onClick} alt="hamburger menu icon" src="http://res.cloudinary.com/lucedesign/image/upload/v1529802602/hamburger_tfga5b.svg" /> : <img onClick={this.props.onClick} alt="close menu icon" src="http://res.cloudinary.com/lucedesign/image/upload/v1529821228/close_k83kph.svg" />
+      this.props.toggle ? <img onClick={this.props.onClick} alt="hamburger menu icon" src="https://res.cloudinary.com/lucedesign/image/upload/v1529802602/hamburger_tfga5b.svg" /> : <img onClick={this.props.onClick} alt="close menu icon" src="http://res.cloudinary.com/lucedesign/image/upload/v1529821228/close_k83kph.svg" />
     );
   }
 }
