1fc6266bf2d0f9daf4275bf8e4f0c6959ec7c3ae
doug-luce <douglasluce@Douglass-MacBook-Pro.local>
2018-07-20 22:20:20 -0700

    Changed hashrouter to browser router in index.js.

--- src/index.js
+++ src/index.js
@@ -2,13 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 // import registerServiceWorker from './registerServiceWorker';
-// import { BrowserRouter as Router } from "react-router-dom";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 
 ReactDOM.render(
-    <HashRouter>
+    <Router>
         <App />
-    </HashRouter>,
+    </Router>,
 
 document.getElementById('root'));
 // registerServiceWorker();
